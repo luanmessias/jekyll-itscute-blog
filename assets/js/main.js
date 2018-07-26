775d7b134aac6ef6ae6552643bf7a3dab69e600c
Luan Messias <luanmessias10@gmail.com>
2018-07-25 22:18:53 -0300

    Depploy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-const home = "aasdasda";
+function scrollBanner() {
+    var scrollPos;
+    var headerText = document.querySelector('.logoarea__wrapper');
+    scrollPos = window.scrollY;
+  
+    if (scrollPos <= 600) {
+        headerText.style.transform =  "translateY(" + (-scrollPos/3) +"px" + ")";
+        headerText.style.opacity = 1 - (scrollPos/600);
+    }
+  }
+  
+  window.addEventListener('scroll', scrollBanner);
