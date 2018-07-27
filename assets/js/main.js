3d1660575eae0b624841dd9876da16b219f0109d
Luan Messias <luanmessias10@gmail.com>
2018-07-26 21:15:37 -0300

    Deply

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,15 @@
 function scrollBanner() {
     var scrollPos;
-    var headerText = document.querySelector('.logoarea__wrapper');
+    var pos = 0;
+    var headerText = document.querySelector('.headerarea__wrapper');
+    var headerBg = document.querySelector('.headerarea');
     scrollPos = window.scrollY;
-  
-    if (scrollPos <= 600) {
-        headerText.style.transform =  "translateY(" + (-scrollPos/3) +"px" + ")";
-        headerText.style.opacity = 1 - (scrollPos/600);
+
+    if (scrollPos <= 500) {
+        headerText.style.transform = "translateY(" + (-scrollPos / 3) + "px" + ")";
+        headerText.style.opacity = 1 - (scrollPos / 500);
+        headerBg.style.backgroundPosition  = "0px " + (scrollPos / 5) + "px";
     }
-  }
-  
-  window.addEventListener('scroll', scrollBanner);
+}
+ 
+window.addEventListener('scroll', scrollBanner);
