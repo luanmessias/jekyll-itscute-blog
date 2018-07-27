ae210484b1b297695ac20ec5d8936ae8c57a19ed
Luan Messias <luanmessias10@gmail.com>
2018-07-27 05:11:48 -0300

    Deploy

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 function scrollBanner() {
     var scrollPos;
-    var pos = 0;
+    var headerHeight = document.querySelector('.headerarea').offsetHeight;
     var headerText = document.querySelector('.headerarea__wrapper');
     var headerBg = document.querySelector('.headerarea');
     scrollPos = window.scrollY;
 
-    if (scrollPos <= 500) {
+    if (scrollPos <= headerHeight) {
         headerText.style.transform = "translateY(" + (-scrollPos / 3) + "px" + ")";
-        headerText.style.opacity = 1 - (scrollPos / 500);
-        headerBg.style.backgroundPosition  = "0px " + (scrollPos / 5) + "px";
+        headerText.style.opacity = 1 - (scrollPos / headerHeight);
+        //headerBg.style.backgroundPosition  = "0px " + (scrollPos / 2) + "px";
     }
 }
  
