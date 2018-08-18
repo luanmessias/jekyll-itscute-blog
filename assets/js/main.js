82daff66bba19347e0fd86dd97b3418b5df04b5e
Luan Messias <luanmessias10@gmail.com>
2018-08-18 15:41:52 -0300

    Deploy

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,7 @@ var feed = new Instafeed({
     userId: '22827060',
     clientId: '716715bc2b8c4d19a26ee83328ccf923',
     accessToken: '22827060.ba4c844.aa2e55c1f79947008c852de12e185569',
-    resolution: 'standard_resolution',
+    resolution: 'low_resolution',
     limit: 6,
     template: '<img class="footer__instagram__feed__img" alt="{{caption}}" src="/assets/img/blank.jpg" data-echo="{{image}}" />'
 });
