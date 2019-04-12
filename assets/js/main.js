10f70275aabebccb0bdceb531293f4000c1172b1
luanmessias <luanmessias10@gmail.com>
2019-04-12 10:35:29 -0300

    Deploy

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,12 +29,12 @@ window.addEventListener('scroll', scrollBanner);
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-
+https://api.instagram.com/oauth/authorize/?client_id=716715bc2b8c4d19a26ee83328ccf923&redirect_uri=REDIRECT-URI&response_type=code
 var feed = new Instafeed({
     get: 'user',
     userId: '22827060',
     clientId: '716715bc2b8c4d19a26ee83328ccf923',
-    accessToken: '22827060.ba4c844.aa2e55c1f79947008c852de12e185569',
+    accessToken: '22827060.1677ed0.cfb7e94e62fa4d7f846304f72d496e01',
     resolution: 'low_resolution',
     limit: 6,
     //template: '<img class="footer__instagram__feed__img" alt="{{caption}}" src="/assets/img/blank.jpg" data-echo="{{image}}" orientation="{{square}}" />'
